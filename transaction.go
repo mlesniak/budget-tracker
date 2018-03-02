c5b17978672193e899536bbd7a7cf51c4e04af01
Michael Lesniak <mail@mlesniak.com>
2018-03-02 06:07:55 +0100

    Move model and function into own file
    Closes #2

---
+++ transaction.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/shopspring/decimal"
+)
+
+// A Transaction describes a single income or expense.
+type Transaction struct {
+	Category  string
+	Timestamp time.Time
+	Amount    decimal.Decimal
+}
+
+func (t Transaction) String() string {
+	return fmt.Sprintf("(%v) %-10v %v",
+		t.Timestamp.Format("2006-01-02 15:04:05"), t.Category, t.Amount)
+}
+
+// Add two Transaction together.
+// Note that the category is empty after that.
+func (t Transaction) Add(t2 Transaction) Transaction {
+	tn := Transaction{}
+	tn.Timestamp = time.Now()
+	tn.Amount = t.Amount.Add(t2.Amount)
+	return tn
+}
+
+// NewTransaction creates a new transaction with the current timestamp.
+func NewTransaction(category string, amount string) Transaction {
+	d, _ := decimal.NewFromString(amount)
+	return Transaction{category, time.Now(), d}
+}
--- transaction_test.go
+++ transaction_test.go
@@ -11,5 +11,13 @@ func TestAdd(t *testing.T) {
 	if t3.Amount.StringFixed(2) != "0.00" {
 		t.Error(t3.Amount)
 	}
+}
 
+func TestAdd2(t *testing.T) {
+	t1 := NewTransaction("income1", "10.34")
+	t2 := NewTransaction("income2", "15.12")
+	t3 := t1.Add(t2)
+	if t3.Amount.StringFixed(2) != "25.46" {
+		t.Error(t3.Amount)
+	}
 }
