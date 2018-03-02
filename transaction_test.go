ffec1fe32b36941b66daa64f02fb093581126083
Michael Lesniak <mail@mlesniak.com>
2018-03-02 06:30:08 +0100

    Add additional tests
    Closes #3

--- transaction_test.go
+++ transaction_test.go
@@ -4,13 +4,33 @@ import (
 	"testing"
 )
 
-func TestBalance(t *testing.T) {
+func TestBalanceBasic(t *testing.T) {
+	t1 := NewTransaction("income", "12.34")
+	t2 := NewTransaction("expense", "-45.67")
+	ta := make([]Transaction, 2)
+	d := append(ta, t1, t2)
+
+	b := ComputeBalance(d)
+	if b.Amount.StringFixed(2) != "-33.33" {
+		t.Error(b.Amount)
+	}
+}
+
+func TestBalanceZero(t *testing.T) {
 	t1 := NewTransaction("income", "10.0")
 	t2 := NewTransaction("expense", "-10.0")
 	ta := make([]Transaction, 2)
 	d := append(ta, t1, t2)
 
 	b := ComputeBalance(d)
+	if b.Amount.StringFixed(2) != "0.00" {
+		t.Error(b.Amount)
+	}
+}
+
+func TestBalanceEmpty(t *testing.T) {
+	ta := make([]Transaction, 2)
+	b := ComputeBalance(ta)
 	if b.Amount.StringFixed(2) != "0.00" {
 		t.Error(b.Amount)
 	}
