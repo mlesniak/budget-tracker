f9c2b54795754694a1bcd416a15b94ca6910a4b3
Michael Lesniak <mail@mlesniak.com>
2018-03-08 06:59:38 +0100

    Add test for persistence
    Closes #6

--- storage_test.go
+++ storage_test.go
@@ -2,15 +2,36 @@ package main
 
 import (
 	"fmt"
+	"os"
 	"testing"
 )
 
+func TestMain(m *testing.M) {
+	InitalizeStorage("./_test.db")
+	code := m.Run()
+	// defer ... will not work here since os.Exit() terminates everything.
+	os.Remove("./_test.db")
+	os.Exit(code)
+}
+
 func TestSave(t *testing.T) {
 	defer RestoreTime(Time)
+	MockTime("2018-03-02 00:00:00")
+	Save(NewTransaction("income2", "5678"))
 	MockTime("2018-03-01 00:00:00")
-	tn := NewTransaction("income", "1234")
-	Save(tn)
-
+	Save(NewTransaction("income1", "1234"))
+	
 	tns := Load(2018, 3)
-	fmt.Println(tns)
+	for _, t := range tns {
+		fmt.Println(t)
+	}
+	if len(tns) != 2 {
+		t.Fatal("Wrong number of transactions")
+	}
+	if tns[0].Category != "income1"  {
+		t.Fatal("income1 not first")
+	}
+	if tns[1].Category != "income2"  {
+		t.Fatal("income2 not second")
+	}
 }
