9271708ba4a9146409e80d7e2e7ff13ed276f389
Michael Lesniak <mail@mlesniak.com>
2018-03-14 05:42:33 +0100

    Add endpoint for budget calculation
    Issue #4

--- main.go
+++ main.go
@@ -17,6 +17,7 @@ func main() {
 func startServer() {
 	r := mux.NewRouter()
 	r.HandleFunc("/api/transaction/{year}/{month}", listHandler)
+	r.HandleFunc("/api/transaction/{year}/{month}/budget", budgetHandler)
 	r.HandleFunc("/api/transaction", postHandler).
 		Methods("POST")
 	port := ":8080"
@@ -45,6 +46,29 @@ func listHandler(w http.ResponseWriter, r *http.Request) {
 	ts := Load(year, month)
 	enc := json.NewEncoder(w)
 	enc.Encode(ts)
+}
+
+func budgetHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	log.Println("Budget transactions handler called. vars=", vars)
+	year, err := strconv.Atoi(vars["year"])
+	if err != nil {
+		log.Println("Unable to parse year", err)
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	month, err := strconv.Atoi(vars["month"])
+	if err != nil {
+		log.Println("Unable to parse month", err)
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	w.Header()["Content-Type"] = []string{"application/json"}
+	ts := Load(year, month)
+	budget := ComputeBudget(ts)
+	enc := json.NewEncoder(w)
+	enc.Encode(budget)
 }
 
 func postHandler(w http.ResponseWriter, r *http.Request) {
