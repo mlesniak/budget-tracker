ecc2cf6dbb6a648a851ac9e33e58e26d828c3879
Michael Lesniak <mail@mlesniak.com>
2018-03-19 06:26:52 +0100

    Show formatted date

--- static/main.js
+++ static/main.js
@@ -50,7 +50,18 @@ var app = new Vue({
                 self.fetchBudget();
                 self.fetchTransactions();
             });
-        }
+        },
+
+        getFormattedDate(date) {
+            var year = date.getFullYear();
+            var month = (1 + date.getMonth()).toString();
+            month = month.length > 1 ? month : '0' + month;
+            var day = date.getDate().toString();
+            day = day.length > 1 ? day : '0' + day;
+            return year + '-' + month + '-' + day;
+          }
+          
+          
     }
 })
 
