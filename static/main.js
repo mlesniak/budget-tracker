1d970f535e9ab0ba6eac5154eb39bb37dfbcbdee
Michael Lesniak <mail@mlesniak.com>
2018-03-31 16:05:41 +0200

    Refactor transactions

--- static/main.js
+++ static/main.js
@@ -4,6 +4,22 @@ Vue.component('budget-display', {
 });
 
 
+Vue.component('transactions', {
+    template: "#transactions",
+    props: ["transactions"],
+    methods: {
+        getFormattedDate(date) {
+            var year = date.getFullYear();
+            var month = (1 + date.getMonth()).toString();
+            month = month.length > 1 ? month : '0' + month;
+            var day = date.getDate().toString();
+            day = day.length > 1 ? day : '0' + day;
+            return year + '-' + month + '-' + day;
+        }
+    }
+});
+
+
 Vue.component('transaction-input', {
     template: "#transaction-input",
     data: function () {
@@ -38,11 +54,7 @@ var app = new Vue({
     el: '#app',
     data: {
         transactions: [],
-        budget: {},
-
-        // For entering a new transaction.
-        amount: "",
-        description: ""
+        budget: {}
     },
     created() {
         this.$on('update', function() {
@@ -53,32 +65,16 @@ var app = new Vue({
     },
 
     methods: {
-        isIncome(index) {
-            return this.transactions[index].amount > 0;
-        },
-
         fetchTransactions() {
             axios.get('/api/transaction/2018/3').then(response => {
                 this.transactions = response.data;
             });
         },
-
         fetchBudget() {
             axios.get('/api/transaction/2018/3/budget').then(response => {
                 this.budget = response.data;
             });
         },
-
-        getFormattedDate(date) {
-            var year = date.getFullYear();
-            var month = (1 + date.getMonth()).toString();
-            month = month.length > 1 ? month : '0' + month;
-            var day = date.getDate().toString();
-            day = day.length > 1 ? day : '0' + day;
-            return year + '-' + month + '-' + day;
-        }
-
-
     }
 })
 
